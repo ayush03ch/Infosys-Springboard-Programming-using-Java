068cec348401fdef5ef8b217cd920a7e46737cb2
Ayush Chaurasiya <114729778+ayush03ch@users.noreply.github.com>
2024-06-03 10:53:04 +0530

    Create Exercise-1.java

---
+++ Introduction to Java/Exercise-1.java
@@ -0,0 +1,7 @@
+class Tester {
+    public static void main(String[] args) {
+        // Implement your code here
+        System.out.println("My name is Ayush Chaurasiya.");
+        System.out.println("My designation is Programming Enthusiast.");
+    }
+}
