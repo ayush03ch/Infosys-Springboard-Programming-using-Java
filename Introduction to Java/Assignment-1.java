fc6de7f6d03dbb4a09b3c85d1059bf949feb51a9
Ayush Chaurasiya <114729778+ayush03ch@users.noreply.github.com>
2024-06-03 10:54:21 +0530

    Create Assignment-1.java

---
+++ Introduction to Java/Assignment-1.java
@@ -0,0 +1,9 @@
+class AddressDetails {
+    public static void main(String[] args) {
+        // Implement your code here
+        System.out.println("Door No: D089");
+        System.out.println("Street: St. Louis Street");
+        System.out.println("City: Springfield");
+        System.out.println("ZIP Code: 62729");
+    }
+}
