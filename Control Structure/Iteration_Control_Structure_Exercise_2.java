92402cbc98e9a146e3db4a1661e74e22d296233f
Ayush Chaurasiya <114729778+ayush03ch@users.noreply.github.com>
2024-06-19 13:29:48 +0530

    Create Iteration_Control_Structure_Exercise_2.java

---
+++ Control Structure/Iteration_Control_Structure_Exercise_2.java
@@ -0,0 +1,11 @@
+class Tester {
+	public static void main(String[] args) {
+		// Implement your code here 
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		
+		for(int i=1; i<n; i++){
+		    System.out.print(i*i + " ");
+		}
+	}
+}
