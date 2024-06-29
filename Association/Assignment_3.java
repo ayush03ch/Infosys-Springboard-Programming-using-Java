bf8ff69091f082e694d0fd36ea6e1128b9f789e8
Ayush Chaurasiya <114729778+ayush03ch@users.noreply.github.com>
2024-06-29 22:41:02 +0530

    Update Assignment_3.java

--- Association/Assignment_3.java
+++ Association/Assignment_3.java
@@ -1,79 +1,126 @@
-class Booking {
-    // Implement your code here
-    private String customerEmail;
-    private int seatsRequired;
-    private boolean isBooked;
-    private static int seatsAvailable;
-
-    static {
-        seatsAvailable = 400;
+class Room {
+    //Implement your code here 
+    private int roomNo;
+    private int capacity;
+    private static int roomCounter;
+    
+    static{
+        roomCounter = 500;
     }
-
-    public Booking(String customerEmail, int seatsRequired) {
-        this.customerEmail = customerEmail;
-        this.seatsRequired = seatsRequired;
-        if (seatsRequired <= seatsAvailable) {
-            this.isBooked = true;
-            seatsAvailable -= seatsRequired;
-        } else {
-            this.isBooked = false;
-        }
+    public static int getRoomCounter(){
+        return Room.roomCounter;
     }
-
-    // Customer Email
-    public void setCustomerEmail(String customerEmail) {
-        this.customerEmail = customerEmail;
+    public static void setRoomCounter(int roomCounter){
+        Room.roomCounter = roomCounter;
     }
-
-    public String getCustomerEmail() {
-        return customerEmail;
+    
+    public Room(){
+        this.roomNo = Room.roomCounter;
+        Room.roomCounter++;
+        this.capacity = 4;
     }
-
-    // Seats Required
-    public void setSeatsRequired(int seatsRequired) {
-        this.seatsRequired = seatsRequired;
+    public int getRoomNo(){
+        return this.roomNo;
     }
-
-    public int getSeatsRequired() {
-        return seatsRequired;
+    public int getCapacity(){
+        return this.capacity;
     }
-
-    // Is Booked
-    public void setBooked(boolean isBooked) {
-        this.isBooked = isBooked;
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
     }
-
-    public boolean isBooked() {
-        return isBooked;
+    
+    //Uncomment the below method after implementation before verifying 
+    //DO NOT MODIFY THE METHOD
+    
+    public String toString(){
+        return "Room\nroomNo: "+this.roomNo+"\ncapacity: "+this.capacity;
     }
-
-    // Seats Available
-    public static void setSeatsAvailable(int seatsAvailable) {
-        Booking.seatsAvailable = seatsAvailable;
-    }
-
-    public static int getSeatsAvailable() {
-        return seatsAvailable;
-    }
+    
 }
+
+class Member {
+	//Implement your code here 
+	
+	private int memberId;
+	private String name;
+	private Room room;
+	
+	public Member(int memberId, String name){
+	    this.memberId = memberId;
+	    this.name = name;
+	}
+	public void setMemberId(int memberId){
+	    this.memebrId = memberId;
+	}
+	public int getMemberId(){
+	    return this.memebrId;
+	}
+	public void setName(String name){
+	    this.name = name;
+	}
+	public String getName(){
+	    return this.name;
+	}
+	public void setRoom(Room room){
+	    this.room = room;
+	}
+	public Room getRoom(){
+	    return this.room;
+	}
+	
+	//Uncomment the below method after implementation before verifying 
+    //DO NOT MODIFY THE METHOD
+    
+    public String toString(){
+        return "Member\nmemberId: "+this.memberId+"\nname: "+this.name;
+    }
+    
+}
+
+class Admin {
+	//Implement your code here 
+	public void assignRoom(Room[] rooms, Member member){
+	    for(int i=0; i<rooms.length;i++){
+	        if(room[i].capacity()>0 &&  member!=null){
+	            rooms[i].setCapacity(rooms[i].getCapacity() - 1);
+	            member.setRoom(rooms[i]);
+	            return;
+	        }
+	    }
+	}
+}
+
 
 class Tester {
-    public static void main(String[] args) {
-        Booking booking1 = new Booking("jack@email.com", 100);
-        Booking booking2 = new Booking("jill@email.com", 350);
+	public static void main(String args[]) {
+		Room room1 = new Room();
+		Room room2 = new Room();
+		Room room3 = new Room();
+		Room room4 = new Room();
+		Room room5 = new Room();
 
-        // Create more objects and add them to the bookings array for testing your code
-        Booking booking3 = new Booking("john@email.com", 50);
+		Room[] totalRooms = { room1, room2, room3, room4, room5 };
 
-        Booking[] bookings = { booking1, booking2, booking3 };
+		Admin admin = new Admin();
 
-        for (Booking booking : bookings) {
-            if (booking.isBooked()) {
-                System.out.println(booking.getSeatsRequired() + " seats successfully booked for " + booking.getCustomerEmail());
-            } else {
-                System.out.println("Sorry " + booking.getCustomerEmail() + ", required number of seats are not available!");
-                System.out.println("Seats available: " + Booking.getSeatsAvailable());
-            }
-        }
-    }
-}
+		Member member1 = new Member(101, "Serena");
+		Member member2 = new Member(102, "Martha");
+		Member member3 = new Member(103, "Nia");
+		Member member4 = new Member(104, "Maria");
+		Member member5 = new Member(105, "Eva");
+		
+		Member[] members = { member1, member2, member3, member4, member5 };
+		
+		for (Member member : members) {
+			admin.assignRoom(totalRooms, member);
+			if(member.getRoom()!=null) {
+				System.out.println("Hi "+member.getName()+"! Your room number is "+member.getRoom().getRoomNo());
+			}
+			else {
+				System.out.println("Hi "+member.getName()+"! No room available");
+			}
+		}
+	}
+} 
+ 
+
