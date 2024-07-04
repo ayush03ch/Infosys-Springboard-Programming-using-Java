80a9dc1889fd5a9c0b44c36b51466dad39bc79b0
Ayush Chaurasiya <114729778+ayush03ch@users.noreply.github.com>
2024-07-04 14:33:04 +0530

    Create Assingment_3.java

---
+++ Recursion/Assingment_3.java
@@ -0,0 +1,24 @@
+class Tester {
+
+    public static int findGCD(int num1, int num2) {
+        if (num2 == 0) {
+            return num1;
+        }
+        return findGCD(num2, num1 % num2);
+    }
+    
+    public static int findMax(int num1, int num2) {
+        return (num1 > num2 ? num1 : num2);
+    }
+
+    public static int findMin(int num1, int num2) {
+        return (num1 < num2 ? num1 : num2);
+    }
+
+    public static void main(String args[]) {
+        System.out.println(findGCD(5, 10));
+        System.out.println(findGCD(14, 21));
+        System.out.println(findGCD(100, 200));
+        System.out.println(findGCD(13, 13));
+    }
+}
